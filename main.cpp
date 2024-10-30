a9f0bf55d4e9e779c41e30040cb03bd89487752c
ym0817 <891922806@qq.com>
2024-10-30 08:26:18 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "adc.h"
+#include "adc_dll.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+
+
+
+int main()
+{
+
+
+	std::string cfgPath = "1";
+
+	const std::string weightPath = "best.onnx";
+	std::string nameList = "class.names";
+	int	threadNum = 1;
+
+	LoadADCModel((char*)cfgPath.c_str(), (char*)weightPath.c_str(), (char*)nameList.c_str(), threadNum);
+
+
+	std::string ImgPath = "192_r.JPG";
+	//std::string ImgPath = "E:\\v10\\ADC_Demo\\ADC_Demo\\185-FAB-41.JPG";
+	float ConfTH = 0.7;
+	int objNum = 0;
+	int* ClassIDs = nullptr;
+	float* Sscore = nullptr;
+
+	ADCModelInferenceImage((char*)ImgPath.c_str(), ConfTH, objNum, &ClassIDs, &Sscore);
+
+	for (int i = 0; i < objNum; i++)
+	{
+		std::cout << "ClassID " << ClassIDs[i] << ", score " << Sscore[i] << std::endl;
+
+	}
+
+
+}
